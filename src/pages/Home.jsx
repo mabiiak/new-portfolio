5c5e82e753e7537d0cca70f8fd3e3b5186855fd9
mabiiak <mabiane.polniak@gmail.com>
2022-08-15 11:30:58 -0300

    fix: html

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,7 +35,8 @@ function Home() {
         <p>
           Sou uma Desenvolvedora Full-Stack, apaixonada pelo front.
           Atualmente estudo Desenvolvimento Web na Trybe, onde eu tive meu primeiro contato com tecnologia e pude aplicar meus conhecimentos em projetos práticos.
-          <br /><br />
+          <br />
+          <br />
           Antes de me encontrar como desenvolvedora, trabalhei com vendas em um call center, telefonista em uma agencia bancaria, e também como vendedora em uma loja de enxovais, e com isso pude melhorar minha comunicação e descobrir a satisfação de ajudar alguém com meu trabalho.
         </p>
         <Link to='/projects'>Projetos</Link>
