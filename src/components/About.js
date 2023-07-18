b74de27ab4f6500ff7444b437578fdef70782782
mabiiak <mabiane.polniak@gmail.com>
2023-07-17 22:06:29 -0300

    feat: altera estrutra da home

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,12 @@ function About() {
       <p id='title'>Olá,</p>
       <p>
         Sou uma Desenvolvedora Full-Stack, apaixonada pelo front.
-        Atualmente estudo Desenvolvimento Web na Trybe, onde eu tive meu primeiro contato com tecnologia e pude aplicar meus conhecimentos em projetos práticos.
+        Atualmente trabalho na NextAge, que é uma empresa de desenvolvimento de softwares e aplicativos.
+        Atualmente estou alocada em um projeto sistema de agendamentos para uma clínica médica.
         <br />
+        <br />
+
+        Estudei Desenvolvimento Web na Trybe, onde eu tive meu primeiro contato com tecnologia e pude aplicar meus conhecimentos em projetos práticos.
         <br />
         Antes de me encontrar como desenvolvedora, trabalhei com vendas em um call center, telefonista em uma agência bancária, e também como vendedora em uma loja de enxovais, e com isso pude melhorar minha comunicação e descobrir a satisfação de ajudar alguém com meu trabalho.
       </p>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,16 +9,16 @@ function Contact() {
   return (
     <Informations>
       <img alt='Foto de Mabiane' src={picture} />
-        <p id='name'> Mabiane Polniak </p>
-        <p id='sub-title'> Desenvolvedora Full-Stack </p>
-        <div id="icons">
-          <a href='https://www.linkedin.com/in/mabianepolniak/' target="_blank" rel="noreferrer">
-            <img alt='icone do linkedin' src={lin}  className="icons-img" />
-          </a>
-          <a id='git' href='https://github.com/mabiiak' target="_blank" rel="noreferrer">
-            <img alt='icone do gitbub' src={git}  className="icons-img" />
-          </a>
-        </div>
+      <p id='name'> Mabiane Polniak </p>
+      <p id='sub-title'> Desenvolvedora Full-Stack </p>
+      <div id="icons">
+        <a href='https://www.linkedin.com/in/mabianepolniak/' target="_blank" rel="noreferrer">
+          <img alt='icone do linkedin' src={lin}  className="icons-img" />
+        </a>
+        <a id='git' href='https://github.com/mabiiak' target="_blank" rel="noreferrer">
+          <img alt='icone do gitbub' src={git}  className="icons-img" />
+        </a>
+      </div>
       <Link id='button' to='/projects'>Projetos</Link>
     </Informations>
   )
